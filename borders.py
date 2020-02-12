d41199c4c588aa1230ff18e064313a688b3f5624
BuildTools <unconfigured@null.spigotmc.org>
2020-02-12 19:38:59 +0000

    Suma sin diferencias

--- borders.py
+++ borders.py
@@ -7,11 +7,11 @@ def getborders(img,h):
 
     #Bordes
     for x in range (0, h):
-        up.append(abs(img[x,0]-img[x,1])) #[X,Y]  Superior
-        down.append(abs(img[x,h-1]-img[x,h-2])) #Inferior
-        left.append(abs(img[0,x]-img[1,x])) #Izquierda
-        right.append(abs(img[h-1,x]-img[h-2,x])) #Derecha
-        #Obtiene la diferencia entre los dos pixeles de cada borde
+        up.append(abs(img[x,0])) #[X,Y]  Superior -img[x,1]
+        down.append(abs(img[x,h-1])) #Inferior -img[x,h-2]
+        left.append(abs(img[0,x])) #Izquierda -img[1,x]
+        right.append(abs(img[h-1,x])) #Derecha -img[h-2,x]
+        #Obtiene la diferencia entre los pixeles de cada borde
 
     #Estructura de bordes [[up],[down],[left],[right]]
     bordes.append(up);bordes.append(down);bordes.append(left);bordes.append(right)
