6462b987512809c6d1638e3f4a0e2e2895465dd1
BuildTools <unconfigured@null.spigotmc.org>
2020-02-12 19:24:36 +0000

    Comparar grid completa

--- main.py
+++ main.py
@@ -10,16 +10,25 @@ import copy
 
 recortar() # Llamar funcion de recortado
 
-imgs=cargarImgs()
-temp1=imgs[4].load()
-temp2=imgs[7].load()
-b=getborders(temp1,166)
-b2=getborders(temp2,166)
-print("(Diferencia de bordes) Imagen numero 1")
-print(b)
-print('')
-print("(Diferencia de bordes) Imagen numero 2")
-print(b2)
-print("Suma de cada diferencia (la menor es el borde)")
-resultado = restar_l(b,b2,166)
-print(sumar_subl(resultado,166))
+imgs=cargarImgs() #Carga todas las imagenes en /src/crop
+
+target=imgs[0].load()
+target_b=getborders(target,166)
+
+#Ubicar imagenes adyacentes a 0
+for x in range(1,9):
+    test=imgs[x].load()
+    test_b=getborders(test,166)
+    print("Suma de cada diferencia x= ",x)
+    resultado = restar_l(target_b,test_b,166)
+    print(sumar_subl(resultado,166))
+    print('')
+
+# print("(Diferencia de bordes) Imagen numero 1")
+# print(b)
+# print('')
+# print("(Diferencia de bordes) Imagen numero 2")
+# print(b2)
+# print("Suma de cada diferencia (la menor es el borde)")
+
+
