afaf915ac122979cdb1060965c3d2e2d930d8349
BuildTools <unconfigured@null.spigotmc.org>
2020-02-13 05:35:33 +0000

    Guardar ordernadas

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ targets=[0,1,2,3,4,5,6,7,8] #Lista de objetivos
 
 recortar() # Llamar funcion de recortado
 
-imgs=cargarImgs() #Carga todas las imagenes en /src/crop
+imgs=cargarImgs(False) #Carga todas las imagenes en /src/crop
+imgscolor=cargarImgs(True) #Carga las imagenes con colores
 dim=imgs[0].height; #Altura de las imagenes
 
 #Ubicar imagenes adyacentes a x
@@ -38,3 +39,11 @@ for x in range (0,9):
                 
 print("grid final")
 print(gridlist)
+
+contador=0
+for x in range(0,len(gridlist)):
+    for y in range(0,len(gridlist)):
+        t = Template('./src/orden/$name.jpg') #Path de guardado
+        path = t.substitute(name=contador) #Template string
+        imgscolor[gridlist[x][y]].save(path) #Guardar ordenados
+        contador=contador+1
